beff7fe995275ad549dcc60b19604621d8d133c1
sepans <sepans@sepans.com>
2016-11-26 23:11:40 -0500

    functionality working without storage

--- src/App.js
+++ src/App.js
@@ -2,42 +2,118 @@ import React, { Component } from 'react'
 import './App.css'
 import Player from './Player'
 import Paragraph from './Paragraph'
+
+import AudioFileList from './AudioFileList'
+
 import SpeakingData from './static/speaking.json'
+import GoonData from './static/goon.json'
+
 //import AudioSrc from './static/speaking.mp3'
-const AudioSrc = require("file!./static/speaking.mp3");
-//const AudioSrc = require("file!./static/anthem.ogg");
+// const speakingSrc = require("file!./static/audio/speaking.mp3");
+// const goonSrc = require("file!./static/audio/goon.mp3");
 
+//const SpeakingData = require("./static/speaking.json");
 
-//const LEFT = 'left', RIGHT = 'right', CENTER = 'center'
+const GOON = 'GOON', SPEAKING = 'SPEAKING', SPEAKING_PLUS_GOON = 'SPEAKING_PLUS_GOON', GOON_PLUS_SPEAKING = 'GOON_PLUS_SPEAKING'
 
-const ONE_OR_A = '1 or A', A_PLUS_ONE = 'A + 1', ONE_PLUS_A = '1 + A'
+const PLAY_MODES = [ SPEAKING, GOON, SPEAKING_PLUS_GOON, GOON_PLUS_SPEAKING]
+
+const SCREEN_CONTINUE = 'screen continue'
+const SCREEN_A_BTNS = 'screen A btns'
+const SCREEN_PLAY_PAUSE_BTNS = 'screen play pause btns'
+
+const CHANNEL_BOTH = 'CHANNEL_BOTH', CHANNEL_LEFT = 'CHANNEL_LEFT', CHANNEL_RIGHT = 'CHANNEL_RIGHT'
+
+//const STORAGE_KEY = 'chrsmnn_last_speaking_para';
+
+const initialState ={
+  speakingLoaded: false,
+  storageLoaded: false,
+  playing: true,
+  screenMode: SCREEN_A_BTNS,
+  displaydiv: false,
+  textPaneOpen: false,
+  playMode: GOON,
+  players: [
+    {
+      paragraph: 0,
+      playing: false,
+      pan: 1,
+      time: 0
+
+    },
+    {
+      paragraph: 0,
+      playing: false,
+      pan: -1,
+      time: 0
+
+    }
+
+  ],
+  playedParagraphs: []
+}
 
 class App extends Component {
+
   constructor(props) {
     super(props)
 
-    this.state = {
-      lastParagraph: 10,
-      players: [
-        {
-          playing: false,
-          gainLeft: 0,
-          gainRight: 1,
-          time: 0
+    this.state = initialState;
 
-        },
-        {
-          playing: false,
-          gainLeft: 1,
-          gainRight: 0,
-          time: 0
+    this.sounds = []
+    this.soundParagraphs = [-1, -1]
+    this.timer = []
 
-        }
 
-      ]
+    this.timeIsUp = this.timeIsUp.bind(this)
+    /*
+    this.audioFileList = {
+      'speaking': Array.apply(null, Array(1)).map((d, i) => {
+        const path = `file!./static/audio/speaking_${i}.mp3`
+        console.log('path', path)
+        return require(path)
+      })
     }
+    */
+
+    //const speaking0 = require('file!./static/audio/speaking_0.mp3')
+/*
+    const speakingAudioImports =  Array.apply(null, Array(1)).map((d, i) => {
+        const path = `file!./static/audio/speaking_${i}.mp3`
+        console.log('path', path)
+        return require(path)
+      })
+*/
+  console.log('audiofilelist', AudioFileList)
+
   }
   componentDidMount() {
+
+    this._loadInitialState()//.done()
+    this.speaking = SpeakingData.paragraphs.map(d => {
+      const parts = d.time_min.split(':')
+      const seconds = (+parts[0])*60 + (+parts[1])
+      return {
+        ...d,
+        time: seconds
+
+      }
+    })
+    this.goon = GoonData.paragraphs.map(d => {
+      const parts = d.time_min.split(':')
+      const seconds = (+parts[0])*60 + (+parts[1])
+      return {
+        ...d,
+        time: seconds
+
+      }
+    })
+
+    this.setState({...this.state, speakingLoaded: true});
+
+    /*
+    console.log('SpeakingData', SpeakingData)
     this.speaking = SpeakingData.paragraphs.map(d => {
       const parts = d.time_min.split(':')
       const seconds = (+parts[0])*60 + (+parts[1])
@@ -53,7 +129,24 @@ class App extends Component {
        {...this.state.players[1], time: times[1]},
 
       ]})
+    */
   }
+
+  _loadInitialState() {
+    console.log('INITIAL STATE')
+
+
+    //TODO implement
+
+    console.log('setting state')
+
+    setTimeout(() => {
+      this.setState({...this.state, storageLoaded: true, screenMode: SCREEN_CONTINUE})
+
+    }, 200)
+
+  }
+
   /*
   pan(side) {
     if(side===LEFT) {
@@ -67,87 +160,162 @@ class App extends Component {
     }
   }
   */
-  play(mode) {
-    const {lastParagraph, players} = this.state
-    let rand
-    switch(mode) {
-      case ONE_OR_A:
-        this.setState({
-          ...this.state,
-          players: [
-            {
-              playing: true,
-              gainLeft: 1,
-              gainRight: 1,
-              time: this.speaking[lastParagraph].time
 
-            },
-            {
-              playing: false,
-              gainLeft: 1,
-              gainRight: 1,
-              time: 0
 
-            }
-          ]
-        })
-        break;
-      case ONE_PLUS_A:
-        rand = Math.floor(Math.random() * this.speaking.length)
-        this.setState({
-          ...this.state,
-          players: [
-            {
-              playing: true,
-              gainLeft: 1,
-              gainRight: 0,
-              time: this.speaking[lastParagraph].time
+  componentWillUpdate(nextProps, nextState) {
+    //check if a player is being changed to playing
 
-            },
-            {
-              playing: true,
-              gainLeft: 0,
-              gainRight: 1,
-              time: this.speaking[rand].time
+    // const { players, playing } = this.state
+    // const nextPlayers = nextState.players
+    // const nextPlaying = nextState.playing
 
-            }
-          ]
-        })
-        break;
-      case A_PLUS_ONE:
-        rand = Math.floor(Math.random() * this.speaking.length)
-        this.setState({
-          ...this.state,
-          players: [
-            {
-              playing: true,
-              gainLeft: 1,
-              gainRight: 0,
-              time: this.speaking[lastParagraph].time
-
-            },
-            {
-              playing: true,
-              gainLeft: 0,
-              gainRight: 1,
-              time: this.speaking[rand].time
-
-            }
-          ]
-        })
-        break;
-    }
+    //TODO player?! or in render
   }
+
+
+
   render() {
-    //const firstPar = this.speaking ? this.speaking[0].time : 0
-    //const secondPar = this.speaking ? this.speaking[1].time : 0
-    console.log('STATE', this.state)
-    const players = this.state.players.map((d, i) => {
-      const playerProps = {...d, src: AudioSrc}
+
+    console.log('------ RENDER, state', this.state)
+
+    if(!this.state.storageLoaded || !this.state.speakingLoaded) {
+      return <div/>
+    }
+
+    const { players, playing, screenMode, /*playMode,*/ playedParagraphs} = this.state
+
+
+    const modeBtns = (
+      <div style={{alignItems: 'center'}}>
+        <button   onClick={(e) => this._handleModePlayPressed(PLAY_MODES[0])}>
+          <span className="hover">speaking is difficult</span>
+        </button>
+        <div>or</div>
+        <button onClick={(e) => this._handleModePlayPressed(PLAY_MODES[1])}>
+          <span className="hover">go on, make me</span>
+        </button>
+        <div>or</div>
+        <button onClick={(e) => this._handleModePlayPressed(PLAY_MODES[2])}>
+          <span className="hover">speaking is difficult / go on, make me</span>
+        </button>
+        <div>or</div>
+        <button onClick={(e) => this._handleModePlayPressed(PLAY_MODES[3])}>
+          <span className="hover">go on, make me / speaking is difficult</span>
+        </button>
+      </div>
+    )
+
+    const btnColor = playing ? '#FF0000' : '#00FF00'
+    const textBtn = <button  onClick={(e) => this.showdiv()}>text</button>
+    const playPauseBtns =  (
+      <div style={{flexDirection:'row', alignSelf: 'stretch', padding: 15}}>
+        <div style={{alignItems: 'flex-start', flex: 1 }}>
+          <button key="playpause" style={{backgroundColor: btnColor, width: 20, height: 20, borderRadius: 10 }} onClick={(e) => this.playPause()}>
+
+          </button>
+        </div>
+        <div style={{width: 50}}>{textBtn}</div>
+      </div>
+    )
+
+    const continueBtns = (
+      <div>
+        <button key="continue"
+
+                onClick={(e) => this.continuePlaying()}>
+          resume
+        </button>
+        <div style={{marginTop: 20}}/>
+        <button key="startover"
+
+                onClick={(e) => this.startOverPlaying()}>
+          start again
+        </button>
+      </div>
+
+    )
+    let btns;
+    switch(screenMode) {
+      case SCREEN_A_BTNS:
+        btns = modeBtns;
+        break;
+      case SCREEN_PLAY_PAUSE_BTNS:
+        btns = playPauseBtns;
+        break;
+      case SCREEN_CONTINUE:
+        btns = continueBtns;
+        break;
+      default:
+        btns = <div/>
+        break;
+     //= screenMode===SCREEN_A_BTNS ? modeBtns : playPauseBtns
+    }
+    console.log('player 0 paragraph', players[0].paragraph, 'player 0 paragraph', players[1].paragraph)
+
+
+    const paragraphsUptoNow = playedParagraphs.map((par, i) => {
+      //{text: SPEAKING, num: newPragraphNum, channel: CHANNEL_RIGHT}
+      const textSegments = par.text===GOON ? this.goon : this.speaking
+      const paragraphdiv = textSegments[par.num].text
+      const textAlign = par.channel===CHANNEL_RIGHT ? 'right' : 'left'
+      console.log(par, textAlign)
+      return <div key={`para-${par.text}-${par.num}`} ref={`para-${par.text}-${par.num}`} style={{padding: 20, textAlign: textAlign, color: '#333'}}>{paragraphdiv}</div>
+    })
+
+
+
+    const text = (
+      <div ref="textscroll" /*onContentSizeChange={(contentWidth, contentHeight)=>{ this.scrollContentSizeChanged(contentWidth, contentHeight)}}*/>
+        <div>{paragraphsUptoNow}</div>
+      </div>
+    )
+
+    const playerEls = this.state.players.map((d, i) => {
+      const playerProps = {...d, index: i,
+         src: i===0 ? AudioFileList.speaking[d.paragraph] : AudioFileList.goon[d.paragraph],
+         onFinish: this.timeIsUp,
+         playing: d.playing && this.state.playing
+
+       }
       return (
         <Player {...playerProps} key={i}/>
       )
     })
+
+    return (
+      <div
+        ref="textdrawer"
+        /*
+        type="displace"
+        content={text}
+        styles={{drawer: { padding: 0}}}
+        tapToClose={true}
+        captureGestures={true}
+        acceptPan={true}
+        negotiatePan={true}
+        acceptDoubleTap={true}
+        panCloseMask={0.3}
+        side='right'
+        tweenHandler={(ratio) => ({
+          main: { opacity:(2-ratio)/2 }
+        })}
+        */
+        >
+          <div className="headphones">
+            <div style={{ color: '#000'}}>for headphones</div>
+          </div>
+          <div className='container'>
+            <div style={{alignSelf: 'stretch',}}>
+              {btns}
+            </div>
+          </div>
+        <button  onClick={(e) => this.clearStorage()}>Delete storage</button>
+        {playerEls}
+        <div className="">{text}</div>
+      </div>
+    )
+    /*
+
     return (
       <div className="App">
         <div className="dot" onClick={() => this.play(ONE_OR_A)}>
@@ -160,10 +328,305 @@ class App extends Component {
           <span className="hover">Play 1 + A</span>
         </div>
         {players}
-        <Paragraph data={this.speaking} currentParagraph={this.state.lastParagraph} display={this.state.players[0].playing} />    
+        <Paragraph data={this.speaking} currentParagraph={this.state.lastParagraph} display={this.state.players[0].playing} />
       </div>
     )
+    */
+  }
+
+  timeIsUp(i) {
+
+    const { playMode, playedParagraphs } = this.state
+
+    const textSegments = i===0 ? this.speaking : this.goon
+
+    console.log(' ----- Time is up for ', i, this, this.state, this.state.players, this.state.players[i])
+    if(!this.state.players[i].playing || !this.state.playing) {
+      return
+    }
+
+    if (i===0 && playMode===GOON_PLUS_SPEAKING) {
+
+
+      const newPragraphNum = Math.floor(Math.random() * textSegments.length)
+      const newTime = textSegments[newPragraphNum].time
+
+      console.log('randomizing ', i, ' to ', newPragraphNum)
+
+      playedParagraphs.push({text: SPEAKING, num: newPragraphNum, channel: CHANNEL_RIGHT})
+
+      this.setState({...this.state, players: [
+               { ...this.state.players[0], paragraph: newPragraphNum, time: newTime}, //
+               { ...this.state.players[1]}
+        ],
+        playedParagraphs: playedParagraphs
+      })
+
+
+      // AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({playMode: this.state.playMode,
+      //        player1Paragraph: newPragraphNum, player2Paragraph: this.state.players[1].paragraph,
+      //        playedParagraphs: playedParagraphs
+      //      }))
+
+
+
+    }
+    else if (i===1 && playMode===SPEAKING_PLUS_GOON) {
+
+      const newPragraphNum = Math.floor(Math.random() * textSegments.length)
+      const newTime = textSegments[newPragraphNum].time
+
+      console.log('randomizing ', i, ' to ', newPragraphNum)
+
+      playedParagraphs.push({text: GOON, num: newPragraphNum, channel: CHANNEL_RIGHT})
+
+      this.setState({...this.state, players: [
+               { ...this.state.players[0]}, //
+               { ...this.state.players[1], paragraph: newPragraphNum, time: newTime}
+        ],
+        playedParagraphs: playedParagraphs
+      })
+
+      //this.sounds[i].setCurrentTime(newTime)
+
+
+      // AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({playMode: this.state.playMode,
+      //        player1Paragraph: this.state.players[0].paragraph, player2Paragraph: newPragraphNum,
+      //        playedParagraphs: playedParagraphs
+      //      }))
+
+
+    }
+    else if (i===0) {
+      //TODO if player.paragraph + 1 > thisSegments.length
+      const newPragraphNum = this.state.players[i].paragraph + 1
+      const newTime = textSegments[newPragraphNum].time
+
+      console.log('continue ', i, ' with ', newPragraphNum)
+
+      playedParagraphs.push({text: SPEAKING, num: newPragraphNum, channel: CHANNEL_LEFT}) // or both, does it matter?
+
+      this.setState({...this.state, players: [
+               { ...this.state.players[0], paragraph: newPragraphNum, time: newTime}, //
+               { ...this.state.players[1]}
+        ],
+        playedParagraphs: playedParagraphs
+      })
+
+
+      // AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({playMode: this.state.playMode,
+      //        player1Paragraph: newPragraphNum, player2Paragraph: this.state.players[1].paragraph,
+      //        playedParagraphs: playedParagraphs
+      //      }))
+
+    }
+    else if (i===1) {
+
+      const newPragraphNum = this.state.players[i].paragraph + 1
+      const newTime = textSegments[newPragraphNum].time
+
+      console.log('continue ', i, ' with ', newPragraphNum)
+
+      playedParagraphs.push({text: GOON, num: newPragraphNum, channel: CHANNEL_LEFT}) // or both, does it matter?
+
+      this.setState({...this.state, players: [
+               { ...this.state.players[0]}, //
+               { ...this.state.players[1], paragraph: newPragraphNum, time: newTime}
+        ],
+        playedParagraphs: playedParagraphs
+      })
+
+      // AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({playMode: this.state.playMode,
+      //        player1Paragraph: this.state.players[0].paragraph, player2Paragraph: newPragraphNum,
+      //        playedParagraphs: playedParagraphs
+      //      }))
+
+    }
+
+  }
+
+  playSound(mode) {
+
+   // const { players } = this.state
+    let rand
+
+    switch(mode) {
+      case SPEAKING:
+        this.setState({
+          ...this.state,
+          screenMode: SCREEN_PLAY_PAUSE_BTNS,
+          playMode: mode,
+          players: [
+            {
+              playing: true,
+              pan: 0,
+              time: this.speaking[this.state.players[0].paragraph].time,
+              paragraph: this.state.players[0].paragraph
+            },
+            {
+              playing: false,
+              pan: 0,
+              time: 0,
+              paragraph: this.state.players[1].paragraph
+
+            }
+          ],
+          playedParagraphs: [{text: SPEAKING, num: 0, channel: CHANNEL_BOTH}]
+        })
+
+        break;
+      case GOON:
+        this.setState({
+          ...this.state,
+          screenMode: SCREEN_PLAY_PAUSE_BTNS,
+          playMode: mode,
+          players: [
+            {
+              playing: false,
+              pan: 0,
+              time: 0,
+              paragraph: this.state.players[0].paragraph
+            },
+            {
+              playing: true,
+              pan: 0,
+              time: this.goon[this.state.players[0].paragraph].time,
+              paragraph: this.state.players[1].paragraph
+
+            }
+          ],
+          playedParagraphs: [{text: GOON, num: 0, channel: CHANNEL_BOTH}]
+        })
+        break;
+      case SPEAKING_PLUS_GOON:
+        //if A+1, then A plays straight through on left channel and the paras of 1 are randomised and played on right.
+        rand = Math.floor(Math.random() * this.goon.length)
+        this.setState({
+          ...this.state,
+          screenMode: SCREEN_PLAY_PAUSE_BTNS,
+          playMode: mode,
+          players: [
+            {
+              playing: true,
+              pan: 1,
+              time: 0,
+              paragraph: 0
+
+            },
+            {
+              playing: true,
+              pan: -1,
+              time: this.goon[rand].time,
+              paragraph: rand
+
+            }
+          ],
+          playedParagraphs: [{text: SPEAKING, num: 0, channel: CHANNEL_LEFT},
+                             {text: GOON, num: rand, channel: CHANNEL_RIGHT}]
+        })
+        break;
+      case GOON_PLUS_SPEAKING:
+        //if 1+A, then 1 plays L and A is randomised on R
+        rand = Math.floor(Math.random() * this.speaking.length)
+        this.setState({
+          ...this.state,
+          screenMode: SCREEN_PLAY_PAUSE_BTNS,
+          playMode: mode,
+          players: [
+            {
+              playing: true,
+              pan: -1,
+              time: this.speaking[rand].time,
+              paragraph: rand
+
+            },
+            {
+              playing: true,
+              pan: 1,
+              time: this.goon[this.state.players[1].paragraph].time,
+              paragraph: this.state.players[1].paragraph
+
+            }
+          ],
+          playedParagraphs: [{text: GOON, num: 0, channel: CHANNEL_LEFT},
+                             {text: SPEAKING, num: rand, channel: CHANNEL_RIGHT}]
+        })
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  showText() {
+    console.log('SHOW TEXT')
+    this.refs.textdrawer.open()
+  }
+
+  _handleModePlayPressed(mode) {
+        console.log('Pressed!!!!', mode)
+        this.playSound(mode)
+  }
+
+  playPause() {
+    console.log('playpaused ', this.state.playing)
+    this.setState({...this.state, playing: !this.state.playing})
+  }
+
+  continuePlaying() {
+
+    console.log('recoveredState', this.recoveredState)
+    this.setState(this.recoveredState)
+  }
+
+  startOverPlaying() {
+
+    this.setState({...this.state, screenMode: SCREEN_A_BTNS})
+
+  }
+
+  clearStorage() {
+
+  }
+
+  showdiv() {
+
   }
 }
+
+
+
+/*
+const styles = StyleSheet.create({
+  headphones: {
+    alignItems: 'center',
+    padding: 10,
+
+
+  },
+  or: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    fontWeight: 'normal',
+    fontSize: 14,
+    color: '#000'
+  },
+  normaltext: {
+    fontWeight: 'normal',
+    fontSize: 14,
+    color: '#000'
+  },
+  centered: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+*/
 
 export default App
--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
 class Player extends Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.panMap = {
+      "-1": [1, 0],
+      "0": [1, 1],
+      "1": [0, 1]
+    }
+
+  }
   componentDidMount() {
 
     this.player = this.refs.audio
-    
+
     window.audioContext = window.audioContext||window.webkitAudioContext; //fallback for older chrome browsers
-    
+
     const context = new AudioContext();
     const source = context.createMediaElementSource(this.player)
 
@@ -31,11 +43,13 @@ class Player extends Component {
 
     this.gainL.gain.value = 1;
     this.gainR.gain.value = 1;
-    
+
     if(this.props.playing) {
           this.player.play()
 
     }
+
+    this.player.addEventListener('ended', () => this.props.onFinish(this.props.index))
 
 
   }
@@ -45,27 +59,58 @@ class Player extends Component {
       gainLeft: 1,
       gainRight: 1,
       curTime: 0
-    }    
+    }
   }
   */
-  componentWillReceiveProps(nextProps) {
+  componentWillUpdate(nextProps, nextState) {
+    //console.log('nextProps', nextProps, this.props)
+    /*
     if(nextProps.playing!==this.props.playing) {
       if(nextProps.playing===true) {
-        this.player.play()
+
+        setTimeout(() => this.player.play(), 500) // todo play when ready
+        this.player.addEventListener('ended', () => this.props.onFinish(this.props.index))
       }
       else {
         this.player.pause()
       }
     }
+    */
+
+    /*
+    console.log('prev onFinish',  this.props.onFinish)
+
+    console.log('next onFinish', nextProps.onFinish)
+
+    if(nextProps.onFinish && !this.props.onFinish) {
+        console.log('set on finish')
+        this.player.addEventListener('ended', () => nextProps.onFinish(this.props.index))
+    }
+    else if(!nextProps.onFinish && this.props.onFinish) {
+        this.player.removeEventListener('ended', this.props.onFinish)
+
+
+
+    }
+    */
   }
+
   render() {
 
-    console.log('playe props', this.props)
+    console.log('player props', this.props)
     if(this.gainL && this.gainR) {
-      this.gainL.gain.value = this.props.gainLeft;
-      this.gainR.gain.value = this.props.gainRight;
-      this.player.currentTime = this.props.time
+      this.gainL.gain.value = this.panMap[this.props.pan][0] ;
+      this.gainR.gain.value = this.panMap[this.props.pan][1];
+      //this.player.currentTime = this.props.time
 
+    }
+
+
+    if(this.props.playing && this.player && this.player.paused) {
+       setTimeout(() => this.player.play(), 500) // todo play when ready
+    }
+    else if (!this.props.playing && this.player && !this.player.paused) {
+      this.player.pause()
     }
 
 
@@ -77,12 +122,15 @@ class Player extends Component {
   }
 }
 
+
 Player.propTypes = {
+  index: React.PropTypes.number,
   src: React.PropTypes.string.isRequired,
-  gainLeft: React.PropTypes.number,
-  gainRight: React.PropTypes.number,
+  pan: React.PropTypes.number,
   time: React.PropTypes.number,
+  onFinish: React.PropTypes.func,
   playing: React.PropTypes.boolean
 }
+
 
 export default Player
